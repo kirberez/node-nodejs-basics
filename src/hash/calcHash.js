867b59bc840908285a9d6adb970818b734b12d73
@kirill_berezutskii <o6olgyu@gmail.com>
2025-04-27 22:54:35 +0300

    refactor: modules and hash done

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
+import { createReadStream } from 'node:fs';
+const { createHash } = await import('node:crypto');
+
 const calculateHash = async () => {
-    // Write your code here 
+    const filename = './files/fileToCalculateHashFor.txt';
+    const hash = createHash('sha256');
+
+    const input = createReadStream(filename);
+    input.on('readable', () => {
+        const data = input.read();
+
+        if (data) {
+            hash.update(data);
+        } else {
+            console.log(hash.digest('hex'));
+        }
+    });
 };
 
 await calculateHash();
